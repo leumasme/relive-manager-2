1c2cba4295352b57839893722cfd0688a11ed758
Temm <samumert@gmail.com>
2022-05-08 18:50:40 +0200

    Remove macOs specific behavior

--- src/electron/index.ts
+++ src/electron/index.ts
@@ -53,18 +53,7 @@ const createWindow = () => {
 
 app.on("ready", createWindow);
 
-// those two events are completely optional to subscrbe to, but that's a common way to get the
-// user experience people expect to have on macOS: do not quit the application directly
-// after the user close the last window, instead wait for Command + Q (or equivalent).
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
-});
-
-app.on("activate", () => {
-  if (mainWindow === null) createWindow();
-});
-
-app.on("web-contents-created", (e, contents) => {
+app.on("web-contents-created", (_evt, contents) => {
   logger.info(e);
   // Security of webviews
   contents.on("will-attach-webview", (event, webPreferences, params) => {
