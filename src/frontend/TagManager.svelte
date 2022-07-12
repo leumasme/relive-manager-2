b6b787362cebaf56b2853752888b987806967970
Temm <samumert@gmail.com>
2022-07-12 12:55:00 +0200

    Improve tag suggestions

--- src/frontend/TagManager.svelte
+++ src/frontend/TagManager.svelte
@@ -54,6 +54,10 @@
   import { db, tagForName } from "./database";
   import { selectedVideo } from "./stores";
 
+  function videoHasTag(name: string) {
+    return $selectedVideo!.tags.some((t) => t.name == tagForName(name).name);
+  }
+
   function addTag(ev: KeyboardEvent) {
     let elem = ev.target as HTMLInputElement;
 
@@ -69,26 +73,26 @@
 
     let tag = tagForName(input);
 
-    if ($selectedVideo!.tags.find((t) => t.name == tag.name)) return;
+    if (videoHasTag(tag.name)) return;
 
     $selectedVideo!.tags.push(tag);
     $selectedVideo = $selectedVideo;
     elem.value = "";
     input = "";
-    suggestedTags = db.tags.filter((x) => x.name.includes(input)).slice(0, 7);
+
+    suggestedTags = db.tags.filter((x) => x.name.includes(input) && !videoHasTag(x.name)).slice(0, 7);
   }
 
   // TODO: make suggestedTags properly reactive (use stores in db?)
   // TODO: useful tag suggestion sorting
   let input: string = "";
-  $: suggestedTags = db.tags.filter((x) => x.name.includes(input)).slice(0, 7);
+  $: suggestedTags = db.tags.filter((x) => x.name.includes(input) && !videoHasTag(x.name)).slice(0, 7);
 
   // TODO: Arrow key & enter navigation for suggestions
   type TagClickHandler = svelteHTML.MouseEventHandler<HTMLDivElement>;
   const tagSuggestionClicked: TagClickHandler = (evt) => {
-    console.log("Yay")
     let tag = tagForName(evt.currentTarget.innerHTML);
-    if ($selectedVideo!.tags.some((t) => t.name == tag.name)) return;
+    if (videoHasTag(tag.name)) return;
 
     $selectedVideo!.tags.push(tag);
     $selectedVideo = $selectedVideo;
